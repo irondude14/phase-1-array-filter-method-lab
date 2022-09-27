adf92fffb2b4aee498bf26f9b8506d414beffbea
Vladyslav <vkadyrov@protonmail.com>
2022-09-26 21:45:24 -0400

    commit tree 8278dc0b5297d94f52414becb24e14a5b7dd5c0e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+function findMatching(array, string) {
+
+    return array.filter(element => {
+        return (element === string || element === string.toLowerCase())
+    })
+}
+
+function fuzzyMatch(array, string) {
+
+    return array.filter(element => {
+        return element.startsWith(string)
+    })
+};
+
+
+function matchName(array, string) {
+    
+    // let names = [];
+
+    // array.filter(element => {
+    //     for (const element of array) {
+    //         if (element.name === string) {
+    //             return names.push(element)
+    //         };
+    //     };
+    // });
+    // return names;
+    return array.filter(element => {
+        return element.name === string
+    })
+};
